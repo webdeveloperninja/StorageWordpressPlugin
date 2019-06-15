b3b886a2d251a35536a7bf1516d5c9757745ee52
Your Name <rsmith5901@gmail.com>
2019-06-15 19:40:13 -0400

    sdfafds

---
+++ admin/submenu-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Creates the submenu page for the plugin.
+ * https://code.tutsplus.com/tutorials/creating-custom-admin-pages-in-wordpress-1--cms-26829
+ *
+ * @package Custom_Admin_Settings
+ */
+
+/**
+ * Creates the submenu page for the plugin.
+ *
+ * Provides the functionality necessary for rendering the page corresponding
+ * to the submenu with which this page is associated.
+ *
+ * @package Custom_Admin_Settings
+ */
+class Submenu_Page
+{
+
+    /**
+     * This function renders the contents of the page associated with the Submenu
+     * that invokes the render method. In the context of this plugin, this is the
+     * Submenu class.
+     */
+    public function render()
+    {
+        echo 'This is the basic submenu page.';
+    }
+}
--- azure_storage.php
+++ azure_storage.php
@@ -26,4 +26,30 @@ function enqueue_custom_element_theme()
 add_shortcode('azure-storage', 'add_web_component');
 
 add_action('wp_enqueue_scripts', 'enqueue_custom_element');
+
 add_action('wp_enqueue_scripts', 'enqueue_custom_element_theme');
+
+add_action('admin_menu', 'my_admin_menu');
+
+function my_admin_menu()
+{
+    add_menu_page('My Top Level Menu Example', 'Top Level Menu', 'manage_options', 'myplugin/myplugin-admin-page.php', 'myplguin_admin_page', 'dashicons-tickets', 6);
+}
+
+// Include the dependencies needed to instantiate the plugin.
+foreach (glob(plugin_dir_path(__FILE__) . 'admin/*.php') as $file) {
+    include_once $file;
+}
+
+
+add_action('plugins_loaded', 'tutsplus_custom_admin_settings');
+/**
+ * Starts the plugin.
+ *
+ * @since 1.0.0
+ */
+function tutsplus_custom_admin_settings()
+{
+    $plugin = new Submenu(new Submenu_Page());
+    $plugin->init();
+}
