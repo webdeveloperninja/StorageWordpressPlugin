525f48d9a3b7f6b4bb4b530317543e6e88210615
Your Name <rsmith5901@gmail.com>
2019-06-15 18:06:24 -0400

    asdffdsa

---
+++ azure_storage.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Azure Storage
+ * Plugin URI: https://github.com/webdeveloperninja/StorageWordpressPlugin
+ * Description: Azure Storage
+ * Version: 1.0
+ * Author: Robert Smith
+ */
